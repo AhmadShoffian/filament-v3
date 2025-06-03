0f61f94225b0c30d39c31924651f58c528c1b7f8
AhmadShoffian <ahmadshof45@gmail.com>
2025-06-03 07:46:48 +0700

    update login

--- app/Models/PortalUser.php
+++ app/Models/PortalUser.php
@@ -1,11 +1,23 @@
 <?php
 
+
 namespace App\Models;
-
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Auth\Authenticatable;
 
-class PortalUser extends Model
+class PortalUser extends Model implements AuthenticatableContract
 {
-    use HasFactory;
+    use Authenticatable;
+
+    protected $table = 'users'; // pastikan nama tabel benar
+
+    // tambahkan kolom yang bisa diisi (opsional)
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
 }
