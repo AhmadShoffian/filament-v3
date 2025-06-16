39a014ba2a84e973aac58d65e9550dac0b3daa53
AhmadShoffian <ahmadshof45@gmail.com>
2025-06-16 23:54:39 +0700

    fix chat realtime

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -7,6 +7,7 @@
 namespace App\Models;
 
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -26,6 +27,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Comment extends Model
 {
     use SoftDeletes;
+    use HasFactory;
     protected $table = 'comments';
 
     public $timestamps = true;
